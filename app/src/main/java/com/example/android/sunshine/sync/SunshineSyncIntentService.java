440eb3139c9f9e581bc5f2b2579345c355ad4fd8
TechnoZoom <kapilbakshi167@gmail.com>
2017-01-30 07:40:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncIntentService.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine.sync;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.SunshineDateUtils;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ */
+public class SunshineSyncIntentService extends IntentService implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleApiClient mWearClient;
+    public final String LOG_TAG = SunshineSyncIntentService.class.getSimpleName();
+
+    public static final String WATCH_WEATHER_PATH = "/weather_watch";
+    public static final String HIGH_TEMP = "HIGH_TEMP";
+    public static final String LOW_TEMP = "LOW_TEMP";
+    public static final String WEATHER_ID = "WEATHER_ID";
+
+    double high_temp;
+    double low_temp;
+    int weather_id;
+
+    public static final String[] MAIN_FORECAST_PROJECTION = {
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+    };
+
+    /*
+     * We store the indices of the values in the array of Strings above to more quickly be able to
+     * access the data from our query. If the order of the Strings above changes, these indices
+     * must be adjusted to match the order of the Strings.
+     */
+    public static final int INDEX_WEATHER_DATE = 0;
+    public static final int INDEX_WEATHER_MAX_TEMP = 1;
+    public static final int INDEX_WEATHER_MIN_TEMP = 2;
+    public static final int INDEX_WEATHER_CONDITION_ID = 3;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mWearClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mWearClient.connect();
+        Log.d(LOG_TAG, "google api client connected");
+    }
+
+    @Override
+    public void onDestroy() {
+//        super.onDestroy();
+
+    }
+
+    public SunshineSyncIntentService() {
+
+        super("SunshineSyncIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        SunshineSyncTask.syncWeather(this);
+
+        long normalizedUtcNow = SunshineDateUtils.normalizeDate(System.currentTimeMillis());
+        Uri uriForDateToday = WeatherContract.WeatherEntry.buildWeatherUriWithDate(normalizedUtcNow);
+
+        Cursor cursor = getContentResolver().query(uriForDateToday, MAIN_FORECAST_PROJECTION, null, null, null);
+
+        if(cursor!=null && cursor.moveToFirst()){
+            weather_id = cursor.getInt(INDEX_WEATHER_CONDITION_ID);
+            high_temp = cursor.getDouble(INDEX_WEATHER_MAX_TEMP);
+            low_temp = cursor.getDouble(INDEX_WEATHER_MIN_TEMP);
+            Log.d(LOG_TAG, "google api client cursor fetched weather");
+            updateWearWeather(weather_id, high_temp, low_temp);
+        }
+
+    }
+
+    private void updateWearWeather(int weather_id, double high, double low){
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(WATCH_WEATHER_PATH).setUrgent();
+        putDataMapRequest.getDataMap().putInt(WEATHER_ID, weather_id);
+        putDataMapRequest.getDataMap().putDouble(HIGH_TEMP, high);
+        putDataMapRequest.getDataMap().putDouble(LOW_TEMP, low);
+        PutDataRequest putDataRequest = putDataMapRequest.asPutDataRequest().setUrgent();
+        PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(mWearClient, putDataRequest);
+
+        pendingResult.setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+            @Override
+            public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
+                if (dataItemResult.getStatus().isSuccess()) {
+                    Log.d(LOG_TAG, "Data item set: " + dataItemResult.getDataItem().getUri());
+
+                } else {
+                    // There was an error sending the data
+                    Log.d(LOG_TAG, "There was an error in sending data to watch");
+
+                }
+            }
+
+        });
+        Log.d(LOG_TAG, "in update weather task");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
---
+++ sunshinewear/src/main/java/com/example/sunshinewear/MyWatchFace.java
@@ -0,0 +1,493 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.sunshinewear;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.support.wearable.watchface.WatchFaceStyle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.WindowInsets;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+import java.lang.ref.WeakReference;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Digital watch face with seconds. In ambient mode, the seconds aren't displayed. On devices with
+ * low-bit ambient mode, the text is drawn without anti-aliasing in ambient mode.
+ */
+public class MyWatchFace extends CanvasWatchFaceService {
+    private static final String LOG_TAG = MyWatchFace.class.getSimpleName();
+    private static final Typeface NORMAL_TYPEFACE =
+            Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL);
+
+    /**
+     * Update rate in milliseconds for interactive mode. We update once a second since seconds are
+     * displayed in interactive mode.
+     */
+    private static final long INTERACTIVE_UPDATE_RATE_MS = TimeUnit.SECONDS.toMillis(1);
+
+    /**
+     * Handler message id for updating the time periodically in interactive mode.
+     */
+    private static final int MSG_UPDATE_TIME = 0;
+
+    @Override
+    public Engine onCreateEngine() {
+        return new Engine();
+    }
+
+    private static class EngineHandler extends Handler {
+        private final WeakReference<MyWatchFace.Engine> mWeakReference;
+
+        public EngineHandler(MyWatchFace.Engine reference) {
+            mWeakReference = new WeakReference<>(reference);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MyWatchFace.Engine engine = mWeakReference.get();
+            if (engine != null) {
+                switch (msg.what) {
+                    case MSG_UPDATE_TIME:
+                        engine.handleUpdateTimeMessage();
+                        break;
+                }
+            }
+        }
+    }
+
+    private class Engine extends CanvasWatchFaceService.Engine implements
+            DataApi.DataListener,
+            GoogleApiClient.ConnectionCallbacks,
+            GoogleApiClient.OnConnectionFailedListener {
+
+        final Handler mUpdateTimeHandler = new EngineHandler(this);
+        boolean mRegisteredTimeZoneReceiver = false;
+        Paint mBackgroundPaint;
+        Paint mTextPaint;
+        Paint dateTextPaint;
+        Paint mLowTempPaint;
+        Paint mHighTempPaint;
+        boolean mAmbient;
+        Calendar mCalendar;
+
+        final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mCalendar.setTimeZone(TimeZone.getDefault());
+                invalidate();
+            }
+        };
+
+        float mXOffset;
+        float mYOffset;
+        float mY1Offset;
+        float mLowTextSize;
+        float mHighTextSize;
+        float dp10;
+        float dp5;
+
+        private GoogleApiClient mWearClient;
+
+        public static final String WATCH_WEATHER_PATH = "/weather_watch";
+        public static final String HIGH_TEMP = "HIGH_TEMP";
+        public static final String LOW_TEMP = "LOW_TEMP";
+        public static final String WEATHER_ID = "WEATHER_ID";
+
+        double high_temp;
+        double low_temp;
+        int weather_id;
+
+        private int weatherResourceId = -1;
+
+        Bitmap weatherIconBitmap;
+        /**
+         * Whether the display supports fewer bits for each color in ambient mode. When true, we
+         * disable anti-aliasing in ambient mode.
+         */
+        boolean mLowBitAmbient;
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+
+            setWatchFaceStyle(new WatchFaceStyle.Builder(MyWatchFace.this)
+                    .setCardPeekMode(WatchFaceStyle.PEEK_MODE_VARIABLE)
+                    .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
+                    .setShowSystemUiTime(false)
+                    .setAcceptsTapEvents(true)
+                    .build());
+            Resources resources = MyWatchFace.this.getResources();
+            mYOffset = resources.getDimension(R.dimen.digital_y_offset);
+            mY1Offset = resources.getDimension(R.dimen.digital_y1_offset);
+
+            mLowTextSize = resources.getDimension(R.dimen.low_temp_text_size);
+            mHighTextSize = resources.getDimension(R.dimen.high_temp_text_size);
+
+            dp10 = resources.getDimension(R.dimen.dp10);
+            dp5 = resources.getDimension(R.dimen.dp5);
+
+            mBackgroundPaint = new Paint();
+            mBackgroundPaint.setColor(resources.getColor(R.color.colorPrimary));
+
+            mTextPaint = new Paint();
+            mTextPaint = createTextPaint(resources.getColor(R.color.digital_text));
+
+            dateTextPaint = new Paint();
+            dateTextPaint = createTextPaint(resources.getColor(R.color.light_white));
+
+            mLowTempPaint = new Paint();
+            mLowTempPaint = createTextPaint(resources.getColor(R.color.light_white));
+
+            mHighTempPaint = new Paint();
+            mHighTempPaint = createTextPaint(resources.getColor(R.color.white));
+
+            mCalendar = Calendar.getInstance();
+
+            mWearClient = new GoogleApiClient.Builder(MyWatchFace.this)
+                    .addApi(Wearable.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+
+        @Override
+        public void onDestroy() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
+//            mWearClient.disconnect();
+            super.onDestroy();
+        }
+
+        private Paint createTextPaint(int textColor) {
+            Paint paint = new Paint();
+            paint.setColor(textColor);
+            paint.setTypeface(NORMAL_TYPEFACE);
+            paint.setAntiAlias(true);
+            return paint;
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+
+            if (visible) {
+                mWearClient.connect();
+                registerReceiver();
+
+                // Update time zone in case it changed while we weren't visible.
+                mCalendar.setTimeZone(TimeZone.getDefault());
+//                mWearClient.connect();
+                invalidate();
+            } else {
+                unregisterReceiver();
+            }
+
+            // Whether the timer should be running depends on whether we're visible (as well as
+            // whether we're in ambient mode), so we may need to start or stop the timer.
+            updateTimer();
+        }
+
+        private void registerReceiver() {
+            if (mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = true;
+            IntentFilter filter = new IntentFilter(Intent.ACTION_TIMEZONE_CHANGED);
+            MyWatchFace.this.registerReceiver(mTimeZoneReceiver, filter);
+        }
+
+        private void unregisterReceiver() {
+            if (!mRegisteredTimeZoneReceiver) {
+                return;
+            }
+            mRegisteredTimeZoneReceiver = false;
+            MyWatchFace.this.unregisterReceiver(mTimeZoneReceiver);
+        }
+
+        @Override
+        public void onApplyWindowInsets(WindowInsets insets) {
+            super.onApplyWindowInsets(insets);
+
+            // Load resources that have alternate values for round watches.
+            Resources resources = MyWatchFace.this.getResources();
+            boolean isRound = insets.isRound();
+            mXOffset = resources.getDimension(isRound
+                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
+            float textSize = resources.getDimension(isRound
+                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
+
+            float textSizeSmall = resources.getDimension(R.dimen.digital_text_size_small);
+
+            mTextPaint.setTextSize(textSize);
+            dateTextPaint.setTextSize(textSizeSmall);
+            mLowTempPaint.setTextSize(mLowTextSize);
+            mHighTempPaint.setTextSize(mHighTextSize);
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties) {
+            super.onPropertiesChanged(properties);
+            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            invalidate();
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            if (mAmbient != inAmbientMode) {
+                mAmbient = inAmbientMode;
+                if (mLowBitAmbient) {
+                    mTextPaint.setAntiAlias(!inAmbientMode);
+                    dateTextPaint.setAntiAlias(!inAmbientMode);
+                    mLowTempPaint.setAntiAlias(!inAmbientMode);
+                    mHighTempPaint.setAntiAlias(!inAmbientMode);
+                }
+                invalidate();
+            }
+
+            // Whether the timer should be running depends on whether we're visible (as well as
+            // whether we're in ambient mode), so we may need to start or stop the timer.
+            updateTimer();
+        }
+
+        /**
+         * Captures tap event (and tap type) and toggles the background color if the user finishes
+         * a tap.
+         */
+        @Override
+        public void onTapCommand(int tapType, int x, int y, long eventTime) {
+            switch (tapType) {
+                case TAP_TYPE_TOUCH:
+                    // The user has started touching the screen.
+                    break;
+                case TAP_TYPE_TOUCH_CANCEL:
+                    // The user has started a different gesture or otherwise cancelled the tap.
+                    break;
+                case TAP_TYPE_TAP:
+                    // The user has completed the tap gesture.
+                    // TODO: Add code to handle the tap gesture.
+//                    Toast.makeText(getApplicationContext(), R.string.message, Toast.LENGTH_SHORT)
+//                            .show();
+                    break;
+            }
+            invalidate();
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            // Draw the background.
+            if (isInAmbientMode()) {
+                canvas.drawColor(Color.BLACK);
+            } else {
+                canvas.drawRect(0, 0, bounds.width(), bounds.height(), mBackgroundPaint);
+            }
+
+            // Draw H:MM in ambient mode or H:MM:SS in interactive mode.
+            long now = System.currentTimeMillis();
+            mCalendar.setTimeInMillis(now);
+
+            String text = String.format("%d:%02d", mCalendar.get(Calendar.HOUR), mCalendar.get(Calendar.MINUTE));
+            canvas.drawText(text, mXOffset, mYOffset, mTextPaint);
+
+            //Date string
+            DateFormat dateFormat = new SimpleDateFormat("EEE, MMM d yyyy");
+            Date date = new Date();
+
+            String text2 = String.format("%s", dateFormat.format(date).toUpperCase());
+            canvas.drawText(text2, mXOffset, mYOffset + mY1Offset, dateTextPaint);
+            canvas.drawText("___", mXOffset, mYOffset + mY1Offset + mY1Offset, dateTextPaint);
+
+            Log.d(LOG_TAG, "draw on canvas weather");
+            //draw weather details on canvas
+            drawWeatherOnCanvas(canvas);
+
+        }
+
+        /*
+         * Draws on watch canvas the weather data
+         */
+
+        private void drawWeatherOnCanvas(Canvas canvas){
+
+            weatherResourceId = getIconResourceForWeatherCondition(weather_id);
+            if(weatherResourceId != -1){
+                Log.d(LOG_TAG, String.valueOf(weatherResourceId));
+                Drawable dr = getResources().getDrawable(weatherResourceId, null);
+                weatherIconBitmap = ((BitmapDrawable)dr).getBitmap();
+                float scaleFactor = getResources().getDimension(R.dimen.high_temp_text_size);
+                float scale = scaleFactor/(float) weatherIconBitmap.getHeight();
+                Bitmap weatherIconBitMapScaled = Bitmap.createScaledBitmap(
+                        weatherIconBitmap, (int) (weatherIconBitmap.getWidth()*scale),
+                        (int) (weatherIconBitmap.getHeight()*scale), true
+                );
+                canvas.drawBitmap(weatherIconBitMapScaled, mXOffset, mYOffset + 2*mY1Offset + dp10, null);
+
+                String highTemp = String.format(getResources().getString(R.string.format_temperature), high_temp);
+                canvas.drawText(highTemp, mXOffset + 3*dp10, mYOffset + 2*mY1Offset + 2*dp10 + dp5, mHighTempPaint);
+
+                String lowTemp = String.format(getResources().getString(R.string.format_temperature), low_temp);
+                canvas.drawText(lowTemp, mXOffset + 8*dp10, mYOffset + 2*mY1Offset + 2*dp10 + dp5, mLowTempPaint);
+            }
+        }
+
+        /**
+         * Starts the {@link #mUpdateTimeHandler} timer if it should be running and isn't currently
+         * or stops it if it shouldn't be running but currently is.
+         */
+        private void updateTimer() {
+            mUpdateTimeHandler.removeMessages(MSG_UPDATE_TIME);
+            if (shouldTimerBeRunning()) {
+                mUpdateTimeHandler.sendEmptyMessage(MSG_UPDATE_TIME);
+            }
+        }
+
+        /**
+         * Returns whether the {@link #mUpdateTimeHandler} timer should be running. The timer should
+         * only run when we're visible and in interactive mode.
+         */
+        private boolean shouldTimerBeRunning() {
+            return isVisible() && !isInAmbientMode();
+        }
+
+        /**
+         * Handle updating the time periodically in interactive mode.
+         */
+        private void handleUpdateTimeMessage() {
+            invalidate();
+            if (shouldTimerBeRunning()) {
+                long timeMs = System.currentTimeMillis();
+                long delayMs = INTERACTIVE_UPDATE_RATE_MS
+                        - (timeMs % INTERACTIVE_UPDATE_RATE_MS);
+                mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs);
+            }
+        }
+
+        public int getIconResourceForWeatherCondition(int weatherId) {
+            // Based on weather code data found at:
+            // http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
+            if (weatherId >= 200 && weatherId <= 232) {
+                return R.drawable.ic_storm;
+            }
+            else if (weatherId >= 300 && weatherId <= 321) {
+                return R.drawable.ic_light_rain;
+            }
+            else if (weatherId >= 500 && weatherId <= 504) {
+                return R.drawable.ic_rain;
+            }
+            else if (weatherId == 511) {
+                return R.drawable.ic_snow;
+            }
+            else if (weatherId >= 520 && weatherId <= 531) {
+                return R.drawable.ic_rain;
+            }
+            else if (weatherId >= 600 && weatherId <= 622) {
+                return R.drawable.ic_snow;
+            }
+            else if (weatherId >= 701 && weatherId <= 761) {
+                return R.drawable.ic_fog;
+            }
+            else if (weatherId == 761 || weatherId == 781) {
+                return R.drawable.ic_storm;
+            }
+            else if (weatherId == 800) {
+                return R.drawable.ic_clear;
+            }
+            else if (weatherId == 801) {
+                return R.drawable.ic_light_clouds;
+            }
+            else if (weatherId >= 802 && weatherId <= 804) {
+                return R.drawable.ic_cloudy;
+            }
+            return -1;
+        }
+
+        @Override
+        public void onConnected(@Nullable Bundle bundle) {
+            Wearable.DataApi.addListener(mWearClient, this);
+            Log.d(LOG_TAG, " connected to google api client");
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            Log.d(LOG_TAG, " connection suspended to google api client");
+        }
+
+        @Override
+        public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+            Log.d(LOG_TAG, " failed connection to google api client " + connectionResult.getErrorMessage());
+        }
+
+        @Override
+        public void onDataChanged(DataEventBuffer dataEvents) {
+            Log.d(LOG_TAG, " in data changed");
+            for (DataEvent event : dataEvents) {
+                if (event.getType() == DataEvent.TYPE_CHANGED) {
+                    // DataItem changed
+                    DataItem item = event.getDataItem();
+                    if (item.getUri().getPath().compareTo(WATCH_WEATHER_PATH) == 0) {
+                        Log.d(LOG_TAG, "data changed in if to set");
+                        DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                        weather_id = dataMap.getInt(WEATHER_ID);
+                        high_temp = dataMap.getDouble(HIGH_TEMP);
+                        low_temp = dataMap.getDouble(LOW_TEMP);
+                    }
+                } else if (event.getType() == DataEvent.TYPE_DELETED) {
+                    // DataItem deleted
+                }
+            }
+            invalidate();
+        }
+    }
+}
